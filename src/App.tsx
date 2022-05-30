b015e5827b9136ac995cd0ea9ab7e0cf8d9b1b60
Amanda Lobo <amandalobo.ag@gmail.com>
2022-05-30 18:13:04 -0300

    @dogzeira Implementou a camada Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
 import Login from './pages/Login/Login';
 import Navbar from './componentes/estaticos/navbar/Navbar'
+import Footer from './componentes/estaticos/navbar/footer/Footer'; 
 import React from 'react';
 import {  Link, Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import { Home } from '@material-ui/icons';
+// import { Home } from '@material-ui/icons';
 
 function App() {
   return (
-<<<<<<< HEAD
     <Router>
       <Navbar />
       <div style={{ minHeight: '100vh' }}>
         <Routes> // Antigo Switch
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
+          {/* <Route path="/home" element={<Home />} /> */}
           {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
         </Routes>
       </div>
       <Footer />
     </Router>
-=======
-    <>
-    <Navbar />
-    <Login />
-    </>
-    
-    
->>>>>>> 81f6d740f7388989180274f3286ed231973391de
   );
 }
 
---
+++ src/componentes/estaticos/navbar/footer/Footer.tsx
@@ -0,0 +1,59 @@
+// comando rfce
+import React from 'react';
+import "./Footer.css";
+import { Typography, Box, Grid } from "@material-ui/core";
+import { GitHub } from '@material-ui/icons';
+
+function Footer() {
+  return (
+  <>
+<Grid
+        className="box"
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid alignItems="center" item xs={12}>
+          <Box className="box1">
+            <Box
+              paddingTop={1}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Typography
+                className="tamanho-f box2"
+                variant="h6"
+                align="center"
+                gutterBottom
+                >
+                  Siga-nos para ter acesso ao nosso portfólio: {" "}
+                </Typography>
+              </Box>
+              <Box display="flex" alignItems="center" justifyContent="center">
+                <a href="https://github.com/Biocommerce-gp1" target="_blank">
+                  <GitHub className="box4" />
+                </a>
+              </Box>
+            </Box>
+            <Box className="box">
+              <Box paddingTop={1}>
+                <Typography variant="subtitle2" align="center" className="box2">
+                  © 2022 Copyright:
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="subtitle2" className="box2" align="center">
+                biocommerce.gp01@gmail.com
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+
+</>
+  );
+}
+
+export default Footer;
