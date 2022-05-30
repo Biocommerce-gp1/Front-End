62909bb149b798fa8b6874488c8b7dda3bb7ea56
dogzeira <douglasdanilobutu13@gmail.com>
2022-05-30 17:07:05 -0300

    Implementou a camada Footer

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./componentes/estaticos/footer/Footer";
+import "./App.css";
+
+
+function App() {
+  return (
+    <Router>
+      <Footer />
+    </Router>
+  );
+}
+export default App;
