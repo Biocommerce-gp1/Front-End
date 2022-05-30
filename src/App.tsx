1c63cca3e62f2df2d66d4f2bf832122ac2907072
Amanda Lobo <amandalobo.ag@gmail.com>
2022-05-30 15:44:49 -0300

    Página Sobre nós

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import Login from './pages/Login/Login';
 import React from 'react';
+import {  Link, Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import { Home } from '@material-ui/icons';
 
 function App() {
   return (
-    <Login />
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
