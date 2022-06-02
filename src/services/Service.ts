b7a14d94495dbb62bfc9f5bc922a1522acdffe16
ninaai517 <carolina.aizawa.moreira@gmail.com>
2022-06-02 14:04:38 -0300

    Criando a Service

---
+++ src/services/Service.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const api= axios.create({
+    baseURL:'https://bloggeneration.herokuapp.com'
+})
+
+//cadastro usuário:
+export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
+    const resposta = await api.post(url,dados)
+    setDado(resposta.data)
+}
+
+//login:
+export const login = async(url: any,dados: any,setDado: any) => { 
+    const resposta = await api.post(url,dados)
+    setDado(resposta.data.token)
+}
+
+
+
+
