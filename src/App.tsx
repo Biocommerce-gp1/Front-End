c0adcc91c9bb011313c69e2519419a7415f6b133
Creiton <thalmaicorreia7@gmai.com>
2022-06-13 09:05:43 -0300

    Atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,7 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 import ListaProduto from './componentes/produto/listaProduto/ListaProduto';
 import DeletarProduto from './componentes/produto/deletarProduto/DeletarProduto';
-<<<<<<< HEAD
-import CadastroAdm from './pages/cadastroAdm/CadastroAdm';
-=======
 import CadastroProduto from './componentes/produto/cadastroProduto/CadastroProduto';
-
->>>>>>> f8af7991ca2923e7fa17b3219d54af2d5bea4fc3
 
 function App() {
   return (
@@ -41,12 +36,9 @@ function App() {
           <Route path='/admin' element={<CadastroAdm />} />
           <Route path="/produto" element={<ListaProduto />} />
           <Route path="/deletarProduto/:id" element={<DeletarProduto />} />
-<<<<<<< HEAD
           <Route path="/adm" element={<CadastroAdm />} />
-=======
           <Route path="/formularioProduto/:id" element={<CadastroProduto />} />
           <Route path="/formularioProduto" element={<CadastroProduto />} />
->>>>>>> f8af7991ca2923e7fa17b3219d54af2d5bea4fc3
 
         </Routes>
       </div>
