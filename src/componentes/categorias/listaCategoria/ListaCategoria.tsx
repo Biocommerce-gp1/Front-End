9e22d8acd784d1f57c9481cf61a24628dddc89fb
Amanda Lobo <amandalobo.ag@gmail.com>
2022-06-13 15:01:30 -0300

    Adicionando favicon e titulo no site

--- src/componentes/categorias/listaCategoria/ListaCategoria.tsx
+++ src/componentes/categorias/listaCategoria/ListaCategoria.tsx
@@ -1,24 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { Box, Button, Card, CardActions, CardContent, Typography } from '@material-ui/core';
 import { Link, useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+
 import Categoria from '../../../models/Categoria';
 import { busca } from '../../../services/Service';
+
 import './ListaCategoria.css';
 import { useSelector } from 'react-redux';
 import { TokenState } from '../../../store/tokens/tokensReducer';
 import { toast } from 'react-toastify';
+function ListaCategoria(){
 
-function ListaCategoria() {
-  let navigate = useNavigate();
+    let navigate = useNavigate()
 
-  const [categorias, setCategorias] = useState<Categoria[]>([]);
+  const [categorias, setCategorias] = useState<Categoria[]>([])
 
   const token = useSelector<TokenState, TokenState["tokens"]>(
     (state) => state.tokens
   );
+
   useEffect(() => {
     if (token === "") {
-      toast.error("Você precisa estar logado", {
+      toast.error('Você precisa estar logado', {
         position: "top-right",
         autoClose: 2000,
         hideProgressBar: false,
@@ -35,30 +39,34 @@ function ListaCategoria() {
   async function getCategoria() {
     await busca("/categoria", setCategorias, {
       headers: {
-        Authorization: token,
-      },
-    });
+        'Authorization': token
+      }
+    })
   }
 
   useEffect(() => {
-    getCategoria();
-  }, [categorias.length]);
+    getCategoria()
+  }, [categorias.length])
 
-  return (
-    <>
-     {
-        categorias.map(categoria => (
+    return(
+        <>
+        {
+            categorias.map(categoria =>(
 
-          <Box m={2} >
+<Box m={2} >
             <Card variant="outlined">
               <CardContent>
 
                 <Typography color="textSecondary" gutterBottom>
-                  {categoria.secao}
+                  Categoria
                 </Typography>
 
                 <Typography variant="h5" component="h2">
-                  {categoria.descricao}
+                 { categoria.secao }
+                </Typography>
+
+                <Typography variant="h5" component="h2">
+                 { categoria.descricao }
                 </Typography>
 
               </CardContent>
@@ -66,7 +74,7 @@ function ListaCategoria() {
               <CardActions>
                 <Box display="flex" justifyContent="center" mb={1.5} >
 
-                  <Link to={/formularioCategoria/${categoria.id}} className="text-decorator-none">
+                  <Link to={`/formularioTema/${ categoria.id }`} className="text-decorator-none">
                     <Box mx={1}>
                       <Button variant="contained" className="marginLeft" size='small' color="primary" >
                         Atualizar
@@ -74,7 +82,7 @@ function ListaCategoria() {
                     </Box>
                   </Link>
 
-                  <Link to={/deletarCategoria/${categoria.id}} className="text-decorator-none">
+                  <Link to={`/deletarTema/${ categoria.id }`} className="text-decorator-none">
                     <Box mx={1}>
                       <Button variant="contained" size='small' color="secondary">
                         Deletar
@@ -87,10 +95,10 @@ function ListaCategoria() {
 
             </Card>
           </Box>
-        ))
-      }
-    </>
-  );
+          ))
+          }
+        </>
+    )
 }
 
 export default ListaCategoria;
