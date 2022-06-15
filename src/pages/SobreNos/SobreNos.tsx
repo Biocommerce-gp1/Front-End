b1dd6eb3beb440a0751d7297bb22475d8e8a1dd1
Amanda Lobo <amandalobo.ag@gmail.com>
2022-06-15 03:11:16 -0300

    Estilizando página sobre nós

--- src/pages/SobreNos/SobreNos.tsx
+++ src/pages/SobreNos/SobreNos.tsx
@@ -9,7 +9,6 @@ import { useNavigate } from 'react-router-dom';
 import { TokenState } from '../../store/tokens/tokensReducer';
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
-import CardSobre from '../../componentes/cardSobre/CardSobre';
 
 function SobreNos() {
 
@@ -17,8 +16,57 @@ function SobreNos() {
 
   return (
     <>
-     <CardSobre />
-   </>
+      <main id="sobre">
+        <section>
+
+          <div>
+            <h2>QUEM SOMOS?</h2>
+            <p>Amigos da preservação e do meio ambiente, o Biocommerce nasceu a partir de um sonho: tornar comunidades e cidades mais sustentáveis. Baseado na ODS 11 da ONU, realizamos vendas de produtos sustentáveis de boa qualidade com preços acessíveis, a fim de criar um impacto ambiental positivo nas comunidades, extendendo-se para cidades e municipios. Além de sermos um e-commerce de produtos sustentáveis que sonha com cidades e comunidades melhores, nós também temos o projeto que prevê o plantio de árvores em comunidades usando parte da renda obtida na venda dos produtos, fazendo com que o nosso sonho seja realizado um pouco a cada dia. Mais que uma empresa, somos sonhadores de um mundo melhor para se viver.</p>
+          </div>
+          <img src="https://i.imgur.com/SeIfgGp.png" alt="" />
+        </section>
+
+        <section>
+          <img src="https://i.imgur.com/SeIfgGp.png" alt="" />
+          <div>
+            <h2>NO QUE NOS BASEAMOS?</h2>
+            <p>Objetivo de desenvolvimento sustentável (ODS) 11: Cidades e comunidades sustentáveis</p>
+            <p>
+            <a href="https://brasil.un.org/pt-br/sdgs/11">11.7 </a>
+               Até 2030, proporcionar o acesso universal a espaços públicos seguros, inclusivos, acessíveis e verdes, particularmente para as mulheres e crianças, pessoas idosas e pessoas com deficiência.  
+            </p>
+            
+          </div>
+
+        </section>
+        </main>
+        <section>
+          <div>
+            <h2>MISSÃO</h2>
+            <p>Proporcionar áreas verdes para lazer da comunidade.</p>
+          </div>
+          {/* <img src="" alt="" /> */}
+        </section>
+
+        <section>
+          {/* <img src="" alt="" /> */}
+          <div>
+            <h2>VISÃO</h2>
+            <p>Cooperar com a diminuição das consequências causadas pelos desmatamentos e poluição na sociedade.</p>
+          </div>
+        </section>
+
+        <section>
+          {/* <img src="" alt="" /> */}
+          <div>
+            <h2>VALORES</h2>
+            <p>Ética, inovação e responsabilidade social são valores fundamentais para nós, e nos tornam empaticos, fortes e dinâmicos.</p>
+          </div>
+        </section>
+
+
+      
+    </>
   );
 }
 
