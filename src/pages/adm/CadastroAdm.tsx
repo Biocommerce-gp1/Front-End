aeb7c74fbcc32ac77ae72b6ea39ccddd6c9de416
Amanda Lobo <amandalobo.ag@gmail.com>
2022-06-19 13:03:02 -0300

    Reajustes da page de CadastroAdm

--- src/pages/adm/CadastroAdm.tsx
+++ src/pages/adm/CadastroAdm.tsx
@@ -1,109 +1,204 @@
-import { Box, Button, Grid, MenuItem, TextField, Typography } from '@material-ui/core'
-import { ChangeEvent, useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import User from '../../models/User'
-import { cadastroUsuario, post } from '../../services/Service';
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { Grid, Box, Typography, Button, TextField } from "@material-ui/core";
+import { Link, useNavigate } from "react-router-dom";
+import "./CadastroUsuario.css";
+import User from "../../models/User";
+import { cadastroUsuario } from "../../services/Service";
+import { toast } from "react-toastify";
 
 function CadastroAdm() {
-    let navigate = useNavigate()
+    let navigate = useNavigate();
 
-    const [confirmarSenha, setConfirmarSenha] = useState<String>("")
+    const [confirmarSenha, setConfirmarSenha] = useState<String>("");
 
     const [user, setUser] = useState<User>({
         id: 0,
         nome: "",
         usuario: "",
         senha: "",
-        tipo: ""
-    })
+        tipo: "",
+    });
 
     const [userResult, setUserResult] = useState<User>({
         id: 0,
         nome: "",
         usuario: "",
         senha: "",
-        tipo: ""
-    })
+        tipo: "",
+    });
 
     useEffect(() => {
         if (userResult.id !== 0) {
-            navigate('/login')
+            navigate("/login");
         }
-    }, [userResult])
+    }, [userResult]);
 
     function updatedModel(e: ChangeEvent<HTMLInputElement>) {
         setUser({
             ...user,
-            [e.target.name]: e.target.value
-        })
+            [e.target.name]: e.target.value,
+        });
     }
 
     function confirmarSenhaHandle(e: ChangeEvent<HTMLInputElement>) {
-        setConfirmarSenha(e.target.value)
+        setConfirmarSenha(e.target.value);
     }
 
     async function cadastrar(e: ChangeEvent<HTMLFormElement>) {
-        e.preventDefault()
+        e.preventDefault();
 
         if (confirmarSenha === user.senha && user.senha.length >= 8) {
-
             try {
-                await cadastroUsuario(`/usuarios/cadastrar`, user, setUserResult)
-                alert("Usuário cadastrado com sucesso")
-
+                await cadastroUsuario(`/usuarios/cadastrar`, user, setUserResult);
+                toast.success("Usuário cadastrado com sucesso!", {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: false,
+                    theme: "colored",
+                    progress: undefined,
+                });
             } catch (error) {
-                console.log(`Error: ${error}`)
+                console.log(`Error: ${error}`);
 
-                alert("Tá dando erro no cod")
+
             }
-
         } else {
-            alert('Dados inconsistentes! Por favor, verifique as informações passadas')
+            toast.error("Dados do usuário inconsistentes. Erro ao logar", {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
 
-            setUser({ ...user, senha: "" })
-            setConfirmarSenha("")
+            setUser({ ...user, senha: "" });
+            setConfirmarSenha("");
         }
-
     }
+
     return (
-        <>
-            <Grid container direction='row' justifyContent='center' alignItems='center' className='background'>
-                <Grid item xs={6} className='imagem2'></Grid>
-                <Grid item xs={6} alignItems='center'>
-                    <Box paddingX={10}>
-                        <form onSubmit={cadastrar}>
-                            <Typography variant='h3' gutterBottom component='h3' align='center' className='titulo'>Cadastrar</Typography>
+        <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            className="background"
+        >
 
-                            <TextField onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} name='nome' margin='normal' type='name' required fullWidth />
+            <Grid item xs={12} alignItems="center">
+                <Box paddingX={60}>
+                    <form onSubmit={cadastrar}>
+                        <Typography
+                            variant="h3"
+                            gutterBottom
+                            color="textPrimary"
+                            component="h3"
+                            align="center"
+                            className="textos2"
+                        >
+                            Cadastro
+                        </Typography>
 
-                            <TextField onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} label='E-mail' name='usuario' margin='normal' type='email' required fullWidth />
+                        <TextField
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                            id="nome"
+                            label="Nome"
+                            variant="outlined"
+                            name="nome"
+                            margin="normal"
+                            required
+                            fullWidth
+                            className="formulario"
+                        />
 
-                            <TextField onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='Senha' name='senha' margin='normal' type='password' required fullWidth />
+                        <TextField
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                            id="usuario"
+                            label="E-mail"
+                            variant="outlined"
+                            name="usuario"
+                            margin="normal"
+                            required
+                            fullWidth
+                            className="formulario"
+                        />
 
-                            <TextField onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)} id='confirmarSenha' label='Confirmar senha' name='confirmarSenha' margin='normal'
-                                type='password' required fullWidth />
+                        <TextField
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                            id="senha"
+                            label="Senha"
+                            variant="outlined"
+                            name="senha"
+                            margin="normal"
+                            type="password"
+                            placeholder="mínimo 8 caracteres"
+                            required
+                            fullWidth
+                            className="formulario"
+                        />
 
-                            <TextField onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='tipo' label='Tipo de cadastro' name='tipo' margin='normal' type='tipo' required fullWidth />
+                        <TextField
+                            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                                confirmarSenhaHandle(e)
+                            }
+                            id="confirmarSenha"
+                            label="Confirmar Senha"
+                            variant="outlined"
+                            name="confirmarSenha"
+                            margin="normal"
+                            type="password"
 
-                            <Box marginTop={2} textAlign='center'>
-                                <Link to='/login' className='text-decoration'>
-                                    <Button variant='contained' color='secondary' className='button'>
-                                        Cancelar
-                                    </Button>
-                                </Link>
+                            required
+                            fullWidth
+                            className="formulario"
+                        />
 
-                                <Button type='submit' variant='contained' color='primary' className='button'>
-                                    Cadastrar
+
+                        <TextField
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                            id="tipo"
+                            label="Tipo de Usuário"
+                            variant="outlined"
+                            name="tipo"
+                            margin="normal"
+                            type="tipo"
+                            required
+                            fullWidth
+                            className="formulario"
+                        />
+
+                        <Box marginTop={2} textAlign="center">
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                className="btnCadastrar"
+                            >
+                                Cadastrar
+                            </Button>
+
+                            <Link to="/home" className="text-decorator-none">
+                                <Button variant="contained" className="btnCancelar">
+                                    Cancelar
                                 </Button>
+                            </Link>
+                        </Box>
 
-                            </Box>
-                        </form>
-                    </Box>
-                </Grid>
+                    </form>
+                </Box>
+
+
+
             </Grid>
-        </>
 
-    )
+        </Grid>
+
+    );
 }
 
 export default CadastroAdm;
